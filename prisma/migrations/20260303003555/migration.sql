6f0fa94cdd0b980a0f3f86ba19e4bba7f14cb13a
Nzanzu Lwanzo <nzanzu.lwanzo.work@gmail.com>
2026-03-03 02:36:54 +0200

    setup prisma and SqLite

---
+++ prisma/migrations/20260303003555/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Ticket" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "code" TEXT NOT NULL,
+    "qrCode" TEXT NOT NULL,
+    "createdAt" TEXT NOT NULL
+);
